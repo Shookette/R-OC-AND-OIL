249d29f06ee46887246bd6e00e2f229b625a1a21
Jeremy <viallattej@gmail.com>
2015-10-19 15:55:42 +0200

    v1

--- map.js
+++ map.js
@@ -1,46 +1,155 @@
-var geocoder;
+// Le "centre" (latitude, longitude) des départements français
+var depts_focalpoint = [];
+depts_focalpoint[01] = [46.065375,5.448974];
+depts_focalpoint[02] = [49.453628,3.607794];
+depts_focalpoint[03] = [46.367877,3.141527];
+depts_focalpoint[04] = [44.164191,6.232933];
+depts_focalpoint[05] = [44.656398,6.248195];
+depts_focalpoint[06] = [43.920701,7.177142];
+depts_focalpoint[07] = [44.815342,4.373790];
+depts_focalpoint[08] = [49.698015,4.709221];
+depts_focalpoint[09] = [42.943940,1.501116];
+depts_focalpoint[10] = [48.320135,4.124092];
+depts_focalpoint[11] = [43.054657,2.464672];
+depts_focalpoint[14] = [49.092978,-0.356423];
+depts_focalpoint[24] = [45.142728,0.703240];
+depts_focalpoint[33] = [44.883881,-0.474218];
+depts_focalpoint[40] = [44.009922,-0.698237];
+depts_focalpoint[47] = [44.369254,0.468752];
+depts_focalpoint[64] = [43.187408,-0.881594];
+depts_focalpoint[15] = [45.049891,2.717207];
+depts_focalpoint[43] = [45.085981,3.786243];
+depts_focalpoint[63] = [45.771938,3.186703];
+depts_focalpoint[50] = [49.093557,-1.343196];
+depts_focalpoint[61] = [48.576332,0.057986];
+depts_focalpoint[21] = [47.465594,4.792314];
+depts_focalpoint[58] = [47.119579,3.538374];
+depts_focalpoint[71] = [46.655721,4.544038];
+depts_focalpoint[89] = [47.855299,3.594307];
+depts_focalpoint[22] = [48.458409,-2.787477];
+depts_focalpoint[29] = [48.232757,-4.264126];
+depts_focalpoint[35] = [48.172063,-1.652918];
+depts_focalpoint[56] = [47.744269,-2.884961];
+depts_focalpoint[18] = [47.024891,2.426636];
+depts_focalpoint[28] = [48.447422,1.374955];
+depts_focalpoint[36] = [46.812255,1.536098];
+depts_focalpoint[37] = [47.223410,0.709267];
+depts_focalpoint[41] = [47.659764,1.414145];
+depts_focalpoint[45] = [47.913833,2.320001];
+depts_focalpoint[51] = [48.961390,4.218025];
+depts_focalpoint[52] = [48.132939,5.259089];
+depts_focalpoint[20] = [42.056269,9.150663];
+depts_focalpoint[25] = [47.066857,6.380444];
+depts_focalpoint[39] = [46.782985,5.729471];
+depts_focalpoint[70] = [47.638395,6.096119];
+depts_focalpoint[90] = [47.624825,6.950104];
+depts_focalpoint[27] = [49.075729,1.049821];
+depts_focalpoint[67] = [48.599187,7.586570];
+depts_focalpoint[68] = [47.865798,7.222762];
+depts_focalpoint[75] = [48.858853,2.347005];
+depts_focalpoint[76] = [49.661630,0.928599];
+depts_focalpoint[77] = [48.618909,2.975640];
+depts_focalpoint[78] = [48.761996,1.837898];
+depts_focalpoint[30] = [43.960103,4.053731];
+depts_focalpoint[34] = [43.592608,3.366918];
+depts_focalpoint[48] = [44.542683,3.490079];
+depts_focalpoint[66] = [42.625705,2.450215];
+depts_focalpoint[19] = [45.343267,1.877855];
+depts_focalpoint[23] = [46.059506,1.992035];
+depts_focalpoint[87] = [45.919213,1.270488];
+depts_focalpoint[54] = [48.956121,6.274396];
+depts_focalpoint[55] = [49.013088,5.371281];
+depts_focalpoint[57] = [49.020719,6.765730];
+depts_focalpoint[88] = [48.163513,6.295955];
+depts_focalpoint[12] = [44.315875,2.645563];
+depts_focalpoint[31] = [43.305402,1.244674];
+depts_focalpoint[32] = [43.695479,0.460501];
+depts_focalpoint[46] = [44.624894,1.596306];
+depts_focalpoint[65] = [43.143377,0.159571];
+depts_focalpoint[81] = [43.791885,2.234733];
+depts_focalpoint[82] = [44.080753,1.368274];
+depts_focalpoint[59] = [50.528903,3.149132];
+depts_focalpoint[62] = [50.513221,2.371636];
+depts_focalpoint[44] = [47.348001,-1.740869];
+depts_focalpoint[49] = [47.389406,-0.559479];
+depts_focalpoint[53] = [48.150766,-0.644357];
+depts_focalpoint[72] = [48.026716,0.234294];
+depts_focalpoint[85] = [46.675889,-1.469182];
+depts_focalpoint[60] = [49.412233,2.427612];
+depts_focalpoint[80] = [49.971149,2.292245];
+depts_focalpoint[13] = [43.542134,5.020405];
+depts_focalpoint[16] = [45.664696,0.242009];
+depts_focalpoint[17] = [45.730391,-0.779816];
+depts_focalpoint[79] = [46.539156,-0.341337];
+depts_focalpoint[86] = [46.612096,0.554250];
+depts_focalpoint[83] = [43.395516,6.294573];
+depts_focalpoint[84] = [44.045319,5.202928];
+depts_focalpoint[26] = [44.729651,5.238637];
+depts_focalpoint[38] = [45.289696,5.550697];
+depts_focalpoint[42] = [45.753789,4.224511];
+depts_focalpoint[69] = [45.880356,4.702795];
+depts_focalpoint[73] = [45.494388,6.403594];
+depts_focalpoint[74] = [46.045179,6.424750];
+depts_focalpoint[91] = [48.530290,2.250067];
+depts_focalpoint[92] = [48.840203,2.241248];
+depts_focalpoint[93] = [48.910792,2.445818];
+depts_focalpoint[94] = [48.774569,2.461979];
+depts_focalpoint[95] = [49.071363,2.101589];
+depts_focalpoint[971] = [16.203622,-61.550668];
+depts_focalpoint[972] = [14.636899,-60.999009];
+depts_focalpoint[973] = [4.050577,-53.041992];
+depts_focalpoint[974] = [-21.149832,55.531162];
+depts_focalpoint[975] = [-21.460706,165.531132];
+depts_focalpoint[976] = [-12.825192,45.155182];
+
 var map;
-var country = "France";
 
 function initMap() {
     geocoder = new google.maps.Geocoder();
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
+        center: {lat: 46.8759851, lng: 2.7971142},
         zoom: 6
     });
-
-    geocoder.geocode( {'address' : country}, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-            map.setCenter(results[0].geometry.location);
-        }
-    });
-
-
 }
 
 function addMarker(data) {
     var myLatLng;
     var marker;
-    for(var i in data) {
-        geolocate(data, i);
+    for(var dep in data) {
+        var value = data[dep];
+        addCircle(dep, value);
     }
 }
 
-function geolocate(data, i) {
-    setTimeout(function(){
-        console.log(i);
-        var address = data[i][0] + " " + data[i][1] + " " + data[i][2];
-        geocoder.geocode( { 'address': address}, function(results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {
-                myLatLng = {lat: results[0].geometry.location.lat(), lng: results[0].geometry.location.lng()};
-                marker = new google.maps.Marker({
-                  position: myLatLng,
-                  map: map,
-                });
-                marker.setMap(map)
-            } else {
-                console.log(google.maps.GeocoderStatus);
-            }
-        });
-    }, 200);
+function addCircle(dep, value) {
+    var color;
+
+    switch(true) {
+        case value > 0.5:
+            color = '#EB0000';
+            break;
+        case value > 0 && value <= 0.5:
+            color = '#9C0D3F';
+            break;
+        case value > -0.5 && value <= 0:
+            color = '#4E1A7F';
+            break;
+        case value <= -0.5:
+            color = '#0028BF';
+            break;
+
+    }
+
+    var latlng = {lat: depts_focalpoint[dep][0], lng: depts_focalpoint[dep][1]};
+
+    var cityCircle = new google.maps.Circle({
+        strokeColor: color,
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: color,
+        fillOpacity: 0.35,
+        map: map,
+        center: latlng,
+        radius: 75000
+    });
 }
--- parser.js
+++ parser.js
@@ -75,159 +75,160 @@ var oilAdjectives = ["court",
 var averageByDepartment = [];
 var reader = new FileReader();
 
-		function findOcAndOil(that){
+function findOcAndOil(that){
 
-			if(that.files && that.files[0]){
-				var reader = new FileReader();
-				reader.onload = function (e) {
-					var output=e.target.result;
-					var data = CSVToArray(output, ";");
-					parser(data);
-				};//end onload()
-				reader.readAsText(that.files[0]);
+	if(that.files && that.files[0]){
+		var reader = new FileReader();
+		reader.onload = function (e) {
+			var output=e.target.result;
+			var data = CSVToArray(output, ";");
+			parser(data);
+		};//end onload()
+		reader.readAsText(that.files[0]);
+	}
+}
+
+function parser(data) {
+	var departments = [];
+	for(var commune in data) {
+
+		var nomCommune = data[commune][1];
+		var codePostal = data[commune][2];
+		var found = false;
+		if(typeof nomCommune != 'undefined') {
+
+			var department = codePostal.substr(0, 2);
+			if(!departments.hasOwnProperty(department)){
+				departments[department] = [];
+				departments[department]['oc'] = 0;
+				departments[department]['oil'] = 0;
+				departments[department]['neutre'] = 0;
 			}
-		}
-
-		function parser(data) {
-			var departments = [];
-			for(var commune in data) {
-
-				var nomCommune = data[commune][1];
-				var codePostal = data[commune][2];
-				var found = false;
-				if(typeof nomCommune != 'undefined') {
-
-					var department = codePostal.substr(0, 2);
-					if(!departments.hasOwnProperty(department)){
-						departments[department] = [];
-						departments[department]['oc'] = 0;
-						departments[department]['oil'] = 0;
-						departments[department]['neutre'] = 0;
-					}
-					for(var adj in oilAdjectives){
-						var reg = new RegExp("("+oilAdjectives[adj].toUpperCase()+'$)');
-						//console.log(reg);
-						if(nomCommune.match(reg) != null) {
-							departments[department]['oil']++;
-							found = true;
-							break;
-						}
-					}
-					if(!found) {
-						for(var adj in ocAdjectives){
-							//var reg = new RegExp('('+ocAdjectives[adj]+')');
-							if(nomCommune.indexOf(ocAdjectives[adj].toUpperCase()) != -1) {
-								departments[department]['oc']++;
-								found = true;
-								break;
-							}
-						}
-					}
-					if(!found) {
-						departments[department]['neutre']++;
+			for(var adj in oilAdjectives){
+				var reg = new RegExp("("+oilAdjectives[adj].toUpperCase()+'$)');
+				//console.log(reg);
+				if(nomCommune.match(reg) != null) {
+					departments[department]['oil']++;
+					found = true;
+					break;
+				}
+			}
+			if(!found) {
+				for(var adj in ocAdjectives){
+					//var reg = new RegExp('('+ocAdjectives[adj]+')');
+					if(nomCommune.indexOf(ocAdjectives[adj].toUpperCase()) != -1) {
+						departments[department]['oc']++;
+						found = true;
+						break;
 					}
 				}
-
 			}
-			averagingByDepartment(departments);
+			if(!found) {
+				departments[department]['neutre']++;
+			}
 		}
 
-		function averagingByDepartment(data) {
-			for(var department in data) {
-				var mean = (data[department]['oc'] - data[department]['oil']) / (data[department]['oc'] + data[department]['oil'] + data[department]['neutre']);
-				averageByDepartment[department] = mean;
-			}
-			console.log(averageByDepartment);
+	}
+	averagingByDepartment(departments);
+	addMarker(averageByDepartment);
+}
+
+function averagingByDepartment(data) {
+	for(var department in data) {
+		var mean = (data[department]['oc'] - data[department]['oil']) / (data[department]['oc'] + data[department]['oil'] + data[department]['neutre']);
+		averageByDepartment[department] = mean;
+	}
+	console.log(averageByDepartment);
+}
+
+
+
+
+
+
+// ref: http://stackoverflow.com/a/1293163/2343
+// This will parse a delimited string into an array of
+// arrays. The default delimiter is the comma, but this
+// can be overriden in the second argument.
+function CSVToArray( strData, strDelimiter ){
+	// Check to see if the delimiter is defined. If not,
+	// then default to comma.
+	strDelimiter = (strDelimiter || ",");
+
+	// Create a regular expression to parse the CSV values.
+	var objPattern = new RegExp(
+		(
+			// Delimiters.
+			"(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+
+			// Quoted fields.
+			"(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+
+			// Standard fields.
+			"([^\"\\" + strDelimiter + "\\r\\n]*))"
+		),
+		"gi"
+		);
+
+
+	// Create an array to hold our data. Give the array
+	// a default empty first row.
+	var arrData = [[]];
+
+	// Create an array to hold our individual pattern
+	// matching groups.
+	var arrMatches = null;
+
+
+	// Keep looping over the regular expression matches
+	// until we can no longer find a match.
+	while (arrMatches = objPattern.exec( strData )){
+
+		// Get the delimiter that was found.
+		var strMatchedDelimiter = arrMatches[ 1 ];
+
+		// Check to see if the given delimiter has a length
+		// (is not the start of string) and if it matches
+		// field delimiter. If id does not, then we know
+		// that this delimiter is a row delimiter.
+		if (
+			strMatchedDelimiter.length &&
+			strMatchedDelimiter !== strDelimiter
+			){
+
+			// Since we have reached a new row of data,
+			// add an empty row to our data array.
+			arrData.push( [] );
+
 		}
 
+		var strMatchedValue;
 
+		// Now that we have our delimiter out of the way,
+		// let's check to see which kind of value we
+		// captured (quoted or unquoted).
+		if (arrMatches[ 2 ]){
 
-
-
-
-	// ref: http://stackoverflow.com/a/1293163/2343
-		// This will parse a delimited string into an array of
-		// arrays. The default delimiter is the comma, but this
-		// can be overriden in the second argument.
-		function CSVToArray( strData, strDelimiter ){
-			// Check to see if the delimiter is defined. If not,
-			// then default to comma.
-			strDelimiter = (strDelimiter || ",");
-
-			// Create a regular expression to parse the CSV values.
-			var objPattern = new RegExp(
-				(
-					// Delimiters.
-					"(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
-
-					// Quoted fields.
-					"(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
-
-					// Standard fields.
-					"([^\"\\" + strDelimiter + "\\r\\n]*))"
-				),
-				"gi"
+			// We found a quoted value. When we capture
+			// this value, unescape any double quotes.
+			strMatchedValue = arrMatches[ 2 ].replace(
+				new RegExp( "\"\"", "g" ),
+				"\""
 				);
 
+		} else {
 
-			// Create an array to hold our data. Give the array
-			// a default empty first row.
-			var arrData = [[]];
+			// We found a non-quoted value.
+			strMatchedValue = arrMatches[ 3 ];
 
-			// Create an array to hold our individual pattern
-			// matching groups.
-			var arrMatches = null;
-
-
-			// Keep looping over the regular expression matches
-			// until we can no longer find a match.
-			while (arrMatches = objPattern.exec( strData )){
-
-				// Get the delimiter that was found.
-				var strMatchedDelimiter = arrMatches[ 1 ];
-
-				// Check to see if the given delimiter has a length
-				// (is not the start of string) and if it matches
-				// field delimiter. If id does not, then we know
-				// that this delimiter is a row delimiter.
-				if (
-					strMatchedDelimiter.length &&
-					strMatchedDelimiter !== strDelimiter
-					){
-
-					// Since we have reached a new row of data,
-					// add an empty row to our data array.
-					arrData.push( [] );
-
-				}
-
-				var strMatchedValue;
-
-				// Now that we have our delimiter out of the way,
-				// let's check to see which kind of value we
-				// captured (quoted or unquoted).
-				if (arrMatches[ 2 ]){
-
-					// We found a quoted value. When we capture
-					// this value, unescape any double quotes.
-					strMatchedValue = arrMatches[ 2 ].replace(
-						new RegExp( "\"\"", "g" ),
-						"\""
-						);
-
-				} else {
-
-					// We found a non-quoted value.
-					strMatchedValue = arrMatches[ 3 ];
-
-				}
-
-
-				// Now that we have our value string, let's add
-				// it to the data array.
-				arrData[ arrData.length - 1 ].push( strMatchedValue );
-			}
-
-			// Return the parsed data.
-			return( arrData );
 		}
+
+
+		// Now that we have our value string, let's add
+		// it to the data array.
+		arrData[ arrData.length - 1 ].push( strMatchedValue );
+	}
+
+	// Return the parsed data.
+	return( arrData );
+}
